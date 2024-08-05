4ff91187c27b94dab9ec5d5251952a3402051f82
Agnija Ravdo <41111594+agnijaravdo@users.noreply.github.com>
2024-08-05 14:57:57 +0300

    create poemHandler for poem fetching and displaying code

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,7 @@
-import getPoem from './api.js';
-import config from './config.js';
-
-import {
-  showLoader,
-  hideLoader,
-  showError,
-  addLineWordAndLetterElements,
-} from './domUtils.js';
+import fetchAndDisplayPoem from './poemHandler.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
   const textContainer = document.querySelector('.text-box');
 
-  showLoader(textContainer);
-
-  try {
-    const poemLines = await getPoem();
-
-    if (Array.isArray(poemLines)) {
-      textContainer.classList.add('text-box-loaded');
-      addLineWordAndLetterElements(poemLines, textContainer);
-    } else {
-      showError(textContainer, config.errorMessage.invalidData);
-    }
-  } catch (error) {
-    showError(textContainer, error.message);
-  } finally {
-    hideLoader(textContainer);
-  }
+  fetchAndDisplayPoem(textContainer);
 });
---
+++ scripts/poemHandler.js
@@ -0,0 +1,24 @@
+import getPoem from './api.js';
+import { showLoader, hideLoader, showError, addLineWordAndLetterElements } from './domUtils.js';
+import config from './config.js';
+
+async function fetchAndDisplayPoem(textContainer) {
+  showLoader(textContainer);
+
+  try {
+    const poemLines = await getPoem();
+
+    if (Array.isArray(poemLines)) {
+      textContainer.classList.add('text-box-loaded');
+      addLineWordAndLetterElements(poemLines, textContainer);
+    } else {
+      showError(textContainer, config.errorMessage.invalidData);
+    }
+  } catch (error) {
+    showError(textContainer, error.message);
+  } finally {
+    hideLoader(textContainer);
+  }
+}
+
+export default fetchAndDisplayPoem;
