c646da6249636ce42ea9dab62616b7dcb123eeda
Agnija Ravdo <41111594+agnijaravdo@users.noreply.github.com>
2024-08-05 14:13:16 +0300

    add divs for each line word and letter

--- scripts/index.js
+++ scripts/index.js
@@ -14,8 +14,22 @@ document.addEventListener('DOMContentLoaded', async () => {
     poemLines.forEach((line, index) => {
       const lineDiv = document.createElement('div');
       lineDiv.id = `line-${index}`;
-      lineDiv.innerText = line;
       textContainer.appendChild(lineDiv);
+      const words = line.split(' ');
+      words.forEach((word, wordIndex) => {
+        const wordSpan = document.createElement('span');
+        wordSpan.id = `word-${index}-${wordIndex}`;
+        lineDiv.appendChild(wordSpan);
+        const wordWithSpace = word + ' ';
+        const letters = wordWithSpace.split('');
+        letters.forEach((letter, letterIndex) => {
+          const letterSpan = document.createElement('span');
+          letterSpan.id = `letter-${index}-${wordIndex}-${letterIndex}`;
+          letterSpan.innerText = letter;
+          wordSpan.appendChild(letterSpan);
+        })
+      });
+
     });
   } else {
     const errorDiv = document.createElement('div');
